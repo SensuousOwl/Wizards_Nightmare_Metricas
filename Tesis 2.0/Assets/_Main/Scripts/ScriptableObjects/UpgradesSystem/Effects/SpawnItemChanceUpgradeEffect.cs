ee807d4ce389e7be698493deb4c9e4fe6cfb2ddd
NFPerera <niko22602@gmail.com>
2023-11-28 15:55:55 -0300

    Critical chance & Spawn item chance upgrade effect added

---
+++ Tesis 2.0/Assets/_Main/Scripts/ScriptableObjects/UpgradesSystem/Effects/SpawnItemChanceUpgradeEffect.cs
@@ -0,0 +1,14 @@
+﻿using _Main.Scripts.PlayerScripts;
+using UnityEngine;
+
+namespace _Main.Scripts.ScriptableObjects.UpgradesSystem.Effects
+{
+    [CreateAssetMenu(menuName = "Main/Upgrades/Effects/SpawnItemChanceUpgradeEffect")]
+    public class SpawnItemChanceUpgradeEffect : UpgradeEffect
+    {
+        public override void ApplyEffect(PlayerModel p_model, float p_valuePercentage)
+        {
+            p_model.StatsController.AddUpgradeStatForPercentage(StatsId.SpawnItemChance, p_valuePercentage);
+        }
+    }
+}
