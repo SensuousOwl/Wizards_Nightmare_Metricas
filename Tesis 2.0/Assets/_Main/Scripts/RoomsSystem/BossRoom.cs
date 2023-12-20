861df0e5fc29eaf3869e4be006dee9279e020611
NFPerera <niko22602@gmail.com>
2023-12-20 16:17:18 -0300

    cositas

--- Tesis 2.0/Assets/_Main/Scripts/RoomsSystem/BossRoom.cs
+++ Tesis 2.0/Assets/_Main/Scripts/RoomsSystem/BossRoom.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using _Main.Scripts.Enemies;
 using _Main.Scripts.Services.MicroServices.EventsServices;
 using UnityEngine;
 
@@ -6,7 +8,7 @@ namespace _Main.Scripts.RoomsSystem
     public class BossRoom : Room
     {
         [SerializeField] private GameObject activatePassLevel;
-
+        [SerializeField] private List<EnemyModel> bossesToSpawn;
         private void Start()
         {
             activatePassLevel.SetActive(false);
@@ -15,7 +17,7 @@ namespace _Main.Scripts.RoomsSystem
         protected override void EnterPlayerInRoom()
         {
             CloseDoors();
-            EventService.DispatchEvent(new SpawnBossInRoom(this));
+            EventService.DispatchEvent(new SpawnBossInRoom(this,bossesToSpawn ));
         }
 
         public override void ClearRoom()
@@ -28,10 +30,11 @@ namespace _Main.Scripts.RoomsSystem
     public struct SpawnBossInRoom : ICustomEventData
     {
         public Room Room { get; private set; }
-        
-        public SpawnBossInRoom(Room p_room)
+        public List<EnemyModel> Bosses { get; private set; }
+        public SpawnBossInRoom(Room p_room, List<EnemyModel> p_bosses)
         {
             Room = p_room;
+            Bosses = p_bosses;
         }
     }
 }
