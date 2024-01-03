e46936b94be0416604c60506d94efd6b4996747f
NFPerera <niko22602@gmail.com>
2024-01-03 14:12:09 -0300

    BossHealthBar

---
+++ Tesis 2.0/Assets/_Main/Scripts/Enemies/BossHealthBarController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using _Main.Scripts.PlayerScripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Main.Scripts.Enemies
+{
+    public class BossHealthBarController : MonoBehaviour
+    {
+        [SerializeField] private GameObject healthBar;
+        [SerializeField] private Slider healthBarFillObj;
+        [SerializeField] private Slider easeHealthBarFillObj;
+        [SerializeField] private float easeSpeed = 0.05f;
+
+        private float currHp;
+        private bool updateBar;
+
+        private void Start()
+        {
+            healthBar.SetActive(false);
+        }
+
+        public void Initialize(HealthController controller)
+        {
+            healthBar.SetActive(true);
+            controller.OnChangeHealth += UpdateHpBar;
+        }
+        
+        public void Deactivate(HealthController controller)
+        {
+            healthBar.SetActive(false);
+            controller.OnChangeHealth -= UpdateHpBar;
+        }
+
+        private void Update()
+        {
+            if(!updateBar)
+                return;
+            
+            if(Math.Abs(healthBarFillObj.value - easeHealthBarFillObj.value) > 0.05f)
+            {
+                easeHealthBarFillObj.value = Mathf.Lerp(easeHealthBarFillObj.value, currHp, easeSpeed);
+            }
+            else
+            {
+                updateBar = false;
+            }
+        }
+
+        public void UpdateHpBar(float p_maxHp, float p_currHp)
+        {
+            var hpPercentaje = (p_currHp / p_maxHp);
+            healthBarFillObj.value = hpPercentaje;
+            updateBar = true;
+            currHp = p_currHp;
+        }
+        
+    }
+}
---
+++ Tesis 2.0/Assets/_Main/Scripts/Enemies/FSMStates/States/BossDieAndDestroyState.cs
@@ -0,0 +1,34 @@
+﻿using _Main.Scripts.FSM.Base;
+using _Main.Scripts.RoomsSystem;
+using UnityEngine;
+
+namespace _Main.Scripts.Enemies.FSMStates.States
+{
+    [CreateAssetMenu(fileName = "BossDieAndDestroyState", menuName = "_main/States/Executions/BossDieAndDestroyState", order = 0)]
+    public class BossDieAndDestroyState : MyState
+    {
+        [SerializeField] private LayerMask RoomLayer;
+        public override void EnterState(EnemyModel p_model)
+        {
+            var col = Physics.OverlapBox(p_model.transform.position, new Vector3(5,5,5), Quaternion.identity, RoomLayer);
+            BossRoom room = null;
+            for (int i = 0; i < col.Length; i++)
+            {
+                if (col[i].TryGetComponent(out BossRoom l_room))
+                {
+                    room = l_room;
+                    break;
+                }   
+            }
+
+            var hpBar = room.GetHealthBar();
+            hpBar.Deactivate(p_model.HealthController);
+            
+            p_model.TriggerDieEvent();
+            Destroy(p_model.gameObject);
+        }
+        public override void ExecuteState(EnemyModel p_model)
+        {
+        }
+    }
+}
