1c19125b522fd7e8fef5c37d1f289f3166170511
NFPerera <niko22602@gmail.com>
2024-05-15 12:02:45 -0300

    Fade animation for currency

--- Tesis 2.0/Assets/_Main/Scripts/UI/ShowCurrencyUI.cs
+++ Tesis 2.0/Assets/_Main/Scripts/UI/ShowCurrencyUI.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using _Main.Scripts.Services;
 using _Main.Scripts.Services.CurrencyServices;
 using TMPro;
@@ -9,17 +11,92 @@ namespace _Main.Scripts.UI
     public class ShowCurrencyUI : MonoBehaviour
     {
         [SerializeField] private TMP_Text Text;
+        [SerializeField] private float changeTime=2;
+        [SerializeField] private float fadeTime;
 
-        private static ICurrencyService m_currencyService => ServiceLocator.Get<ICurrencyService>();
+        
+        private static ICurrencyService CurrencyService => ServiceLocator.Get<ICurrencyService>();
+        private int m_currentValue;
         private void Start()
         {
-            m_currencyService.OnCurrencyChange += OnCurrencyChange;
-            Text.text = m_currencyService.GetCurrentGs().ToString();
+            CurrencyService.OnCurrencyChange += OnCurrencyChange;
         }
 
         private void OnCurrencyChange(int p_obj)
         {
-            Text.text = p_obj.ToString();
+            StartCoroutine(ChangeValueGradually(p_obj));
         }
+/*
+        private void OnCurrencyChange(int p_newValue)
+        {
+            Text.alpha = 255;
+            if (m_currentValue < p_newValue)
+            {
+                while (m_currentValue <= p_newValue)
+                {
+                    Text.text = m_currentValue.ToString();
+                    m_currentValue++;
+                }
+
+                m_currentValue = p_newValue;
+            }
+            else
+            {
+                while (m_currentValue >= p_newValue)
+                {
+                    Text.text = m_currentValue.ToString();
+                    m_currentValue--;
+                }
+                m_currentValue = p_newValue;
+            }
+
+            while (Text.alpha<0)
+            {
+                Text.alpha -= Text.alpha * Time.deltaTime * fadeTime;
+            }
+            
+        }
+        */
+        private IEnumerator ChangeValueGradually(int p_newValue)
+        {
+            float l_elapsedTime = 0f;
+            int l_startingValue = m_currentValue;
+
+            while (l_elapsedTime < changeTime)
+            {
+                l_elapsedTime += Time.deltaTime;
+                float l_t = l_elapsedTime / changeTime;
+
+                
+                m_currentValue = (int)Mathf.Lerp(l_startingValue, p_newValue, l_t);
+                Text.text = Mathf.RoundToInt(m_currentValue).ToString();
+
+                yield return null;
+            }
+
+            
+            m_currentValue = p_newValue;
+            Text.text = m_currentValue.ToString();
+
+            
+            StartCoroutine(FadeOutText());
+        }
+
+        private IEnumerator FadeOutText()
+        {
+            float l_elapsedTime = 0f;
+
+            while (l_elapsedTime < fadeTime)
+            {
+                l_elapsedTime += Time.deltaTime;
+                float l_t = l_elapsedTime / fadeTime;
+
+                Text.alpha = Mathf.Lerp(1f, 0f, l_t);
+                yield return null;
+            }
+
+            Text.alpha = 0f;
+        }
+        
     }
 }
